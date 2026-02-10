a1a73db2fb8344994f2e102ceada0040f10cba3d
thbinhkhanh <thbinhkhanh@gmail.com>
2026-02-10 20:11:06 +0700

    fix

--- src/pages/TracNghiem.jsx
+++ src/pages/TracNghiem.jsx
@@ -2124,7 +2124,12 @@ return (
               fontWeight="bold"
               textAlign="center"
               mb={2}
+              fontSize="1.1rem"
               color="#0d47a1"
+              sx={{
+                userSelect: "none",        // ✅ CHẶN BÔI ĐEN
+                cursor: "default",
+              }}
             >
               Câu hỏi
             </Typography>
